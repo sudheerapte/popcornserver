a8c65996e5f306398bb81c4510b6bd9068c38913
Sudheer Apte <sudheerapte@yahoo.com>
2019-01-16 21:04:38 -0500

    launch.js simplified and tightened

--- server/launch.js
+++ server/launch.js
@@ -1,14 +1,17 @@
 "use strict";
 
-const fs = require('fs');
-const path = require('path');
 const registry = require('./registry.js');
 const hsmodule = require('./http-server.js');
 const options = require('./get-options-sync.js');
 
 const machineObj = options.machineDirs || {"demo": "%D/demo"};
 Object.keys(machineObj).forEach( k => {
-  registry.addMachine(k, machineObj[k]);
+  if (k.match(/^[a-z0-9]+$/)) {
+    registry.addMachine(k, machineObj[k]);
+  } else {
+    console.log(`machine name: ${k} not lowercase; ignoring`);
+  }
 });
-let port = options.port || "8000";
-const httpServer = new hsmodule(8000);
+let port = options.httpPort || "8000";
+if (port >= 65536 || port <= 0) { port = 8000; }
+const httpServer = new hsmodule(port);
