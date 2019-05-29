228528d38ad37a1e257f5467d7671bd49aa56a60
Sudheer Apte <sudheerapte@yahoo.com>
2019-05-29 08:50:34 -0400

    doflies.py gets OK and listens for commands

---
+++ bin/doflies.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import socket
+import sys
+import re
+
+HOST, PORT = "localhost", 8001
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes("event: appConnect\ndata: doflies\n\n", "utf-8"))
+    received = str(sock.recv(1024), "utf-8")
+    if re.search("ok", received):
+        print("Received OK")
+        while received:
+            received = str(sock.recv(1024), "utf-8")
+            print("Received {}".format(received))
+print("Popcorn dropped connection.")
+
+adj = {
+    'a': ('b', 'c', 'd' ),
+    'b': ('a', 'c', 'd', 'e' ),
+    'c': ('a', 'b', 'd', 'g' ),
+    'd': ('a', 'b', 'c', 'e', 'f', 'g'),
+    'e': ('b', 'd', 'f', 'h' ),
+    'f': ('d', 'e', 'g', 'h' ),
+    'g': ('c', 'd', 'f', 'h' ),
+    'h': ('e', 'f', 'g' )}
+
+fwd = {
+    'a': ('b', 'c', 'd' ),
+    'b': ('c', 'd', 'e' ),
+    'c': ('b', 'd', 'g' ),
+    'd': ('e', 'f', 'g'),
+    'e': ('f', 'h' ),
+    'f': ('e', 'g', 'h' ),
+    'g': ('f', 'h' ),
+    'h': ( )}
+
+def isadj(x, y):
+    if y in adj[x] :
+        print('yes, {0} is adjacent to {1}'.format(y,x))
+    else:
+        print('no, {0} is NOT adjacent to {1}'.format(y,x))
+
+def isfwd(x, y):
+    if y in fwd[x] :
+        print('yes, {0} is forward from {1}'.format(y,x))
+    else:
+        print('no, {0} is NOT forward from {1}'.format(y,x))
+
+isadj('d', 'e')
+isadj('d', 'g')
+isadj('d', 'h')
+
