223f1e6ca7ad11b52736016d6823dfd976e5c371
Sudheer Apte <sudheerapte@yahoo.com>
2019-10-24 23:04:42 -0400

    provideStep and initStep

--- js/test/runtime-test.js
+++ js/test/runtime-test.js
@@ -12,17 +12,37 @@ const P = new Runtime( s => log(s) );
 
 log(`---- include`);
 
-let providedMachineLines, mc, e;
+let providedMachineLines, mc, e, lines;
+
+function provideStepP() {
+  return new Promise( (resolve, reject) => {
+    mc = new Machine;
+    mc.interpret(providedMachineLines);
+    e = new Executor(mc, t, new Parser(t), log);
+    P.setExecutor(e);
+    P.buildProcsMap(lines);
+    return resolve();
+  });
+}
+
+function initStepP() {
+  return new Promise( (resolve, reject) => {
+    P.execProc("INIT");
+    return resolve();
+  });
+}
 
 providedMachineLines = [
   "addLeaf . a", "addLeaf . b", "addLeaf .a / foo", "addLeaf .a / bar",
 ];
-mc = new Machine;
-mc.interpret(providedMachineLines);
-e = new Executor(mc, t, new Parser(t), log);
-P.setExecutor(e);
+lines = [
+  "%INIT",
+  "",
+];
 
-log('done');
+provideStepP()
+  .then( initStepP )
+  .then( () => log("done") );
 
 
 
