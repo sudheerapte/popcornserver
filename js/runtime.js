180b33b522406625c8671ae1714f044e55eb0a70
Sudheer Apte <sudheerapte@yahoo.com>
2019-10-24 22:56:59 -0400

    runtime first version

--- js/runtime.js
+++ js/runtime.js
@@ -1,31 +1,141 @@
 "use strict";
 
+// Requires Tokenizer, Parser, Machine, Executor
+// We don't require() them here because we send raw bundle to browser
+
 class Runtime {
   constructor(logFunc) {
     this._singletonMap = new Map();
     this._queue = [];
+    this._varDict = {}; // base dictionary
     this.log = logFunc ? logFunc : console.log;
+    this._flowing = true; // automatically executing queued items
   }
-  setSingleton(name, singleton) {
-    this._singletonMap.set(name, singleton);
+  buildProcsMap(lines) {
+    this._e.buildProcsMap(lines);
   }
-  getSingleton(name) {
-    return this._singletonMap.get(name);
-  }
+
+  /**
+     Item queue execution
+
+     Items are event records that can make changes to the state model.
+     You simply enqueue() each item. The Runtime automatically unshifts
+     items from the queue and executes them.
+
+     Item names:
+
+         UPDATE TIMER HANDLER
+
+     On enqueueing, each item is immediately scheduled for execution,
+     unless the setFlowing() setting is set to true.
+
+   */
 
   enqueue(item) {
     this._queue.push(item);
-    setImmediate(() => this.doOne());
+    this.flowIfNeeded();
+  }
+  getFlowing() { return this._flowing; }
+  setFlowing(onOff) {
+    this._flowing = onOff;
+    this.flowIfNeeded();
   }
   doOne() {
     if (this._queue.length > 0) {
       const item = this._queue.unshift();
-      item.execute();
+      this.execute(item);
+    }
+  }
+  execute(item) {
+    if (item.name === 'UPDATE' || item.name === 'TIMER') {
+      this.execLines(item.lines);
+      this.execProc("RENDER");
+    } else if (item.name === 'HANDLER') {
+      this.execProc(item.procName, this.makeVarDictFunc(item.varDict));
+      this.execProc("RENDER");
     }
   }
   getNumItems() {
     return this._queue.length;
   }
+  flowIfNeeded() {
+    if (this._flowing && this._queue.length > 0) {
+      setImmediate(() => this.doOne());
+    }
+  }
+  /**
+     makeVarDictFunc() - Utility function to use temporary dictionary
+
+     Pass this as second argument to execProc or execLines
+   */
+  makeVarDictFunc(tempVarDict) {
+    return this.getVarDictFunc( name => {
+      if (tempVarDict.hasOwnProperty(name)) {
+        return tempVarDict[name];
+      } else {
+        return null;
+      }
+    });
+  }
+
+
+  // get, set base dictionary entries
+  getVar(name) { return this._varDict[name]; }
+  setVar(name, value) { this._varDict.name = value; }
+  
+  /**
+     execProc() and execLines()
+
+     Take optional varDictFunc argument to define some overriding variables.
+     Use the makeVarDictFunc() utility function for convenience.
+   */
+
+  execProc(name, vdf) {
+    const result = this._e.execProc(name, vdf);
+    if (result) { this.log(result); }
+  }
+  execLines(lines, vdf) {
+    const result = this._e.runLines(lines, vdf);
+    if (result) { this.log(result); }
+  }
+
+  getProcsMap() {
+    return this._e.procs;
+  }
+  setExecutor(e) { this._e = e; }
+  setMc(mc) {
+    this._e.setMc(mc);
+  }
+
+  // ----------------- private functions below this point
+  /**
+     getVarDictFunc() - return a function suitable for VAR expansion
+
+     Optionally takes a higher-priority superposedFunc. If the
+     superposedFunc() returns null, then the base varDict is used.
+   */
+  getVarDictFunc(superposedFunc) {
+    if (! this._varDictFunc) { // first time: set member
+      this._varDictFunc = name => {
+        if (this._varDict.hasOwnProperty(name)) {
+          return {name: 'WORD', value: this._varDict[name]};
+        } else {
+          return null;
+        }
+      };
+    }
+    if (! superposedFunc) {
+      return this._varDictFunc;
+    } else {
+      return name => {
+        const result = superposedFunc(name);
+        if (result) { return result; }
+        else return this._varDictFunc(name);
+      };
+    }
+  }
+
+
 }
 
 module.exports = Runtime;
--- js/test/runtime-test.js
+++ js/test/runtime-test.js
@@ -1,13 +1,28 @@
 "use strict";
 
 const [log, err, errDiff] = require('./logerr.js');
+const Machine = require('../machine.js');
 const Runtime = require('../runtime.js');
-const P = new Runtime;
+const Parser = require('../parser.js');
+const Tokenizer = require('../tokenizer.js');
+const t = new Tokenizer;
+const Executor = require('../executor.js');
+
+const P = new Runtime( s => log(s) );
 
 log(`---- include`);
-P.setSingleton("foo", {});
 
-log(P.getSingleton("foo"));
+let providedMachineLines, mc, e;
+
+providedMachineLines = [
+  "addLeaf . a", "addLeaf . b", "addLeaf .a / foo", "addLeaf .a / bar",
+];
+mc = new Machine;
+mc.interpret(providedMachineLines);
+e = new Executor(mc, t, new Parser(t), log);
+P.setExecutor(e);
+
+log('done');
 
 
 
